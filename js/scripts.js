cb63f5afe52aac64eefd4d369557306a80b131c4
Andrey Popov <andpop@mail.ru>
2018-05-16 22:26:17 +0300

    Сделал переходы на секции в главном и гамбургер-меню

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,47 @@
-const hamburgermenu_link = document.querySelector('.hamburger-menu-link');
-const hamburgermenu_content = document.querySelector('.hamburger-menu__content');
-const hamburgermenu_close   = document.querySelector('.hamburger-menu__close');
+
+
+// Обработка полноэкранного гамбургер-меню
+var hamburgermenu_link       = document.querySelector('.hamburger-menu-link');
+var hamburgermenu_content    = document.querySelector('.hamburger-menu__content');
+var hamburgermenu_close      = document.querySelector('.hamburger-menu__close');
+var hamburgermenu_item_links = document.querySelectorAll('.hamburger-menu__link');
 
 hamburgermenu_link.addEventListener('click', function(event) {
   event.preventDefault();
   hamburgermenu_content.style.display = 'flex';
-  // hamburger__menu.style.display = 'none';
-  document.body.style.overflow = 'hidden';
-}) 
+  document.body.style.overflow        = 'hidden';
+});
 
 hamburgermenu_close.addEventListener('click', function(event) {
   event.preventDefault();
   hamburgermenu_content.style.display = '';
-  // hamburger__menu.style.display = '';
-  document.body.style.overflow = '';
-})
+  document.body.style.overflow        = '';
+});
+
+var l = hamburgermenu_item_links.length;
+for (let i = 0; i<l; i++) {
+  hamburgermenu_item_links[i].addEventListener('click', function(event) {
+    hamburgermenu_content.style.display = '';
+    document.body.style.overflow        = '';
+  })
+};
+
+// Обработка аккордеона в секции "Команда"
+var teamacco_items = document.querySelectorAll('.team-acco__item');
+var l              = teamacco_items.length;
+for (let i = 0; i<l; i++) {
+  teamacco_items[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (teamacco_items[i].classList.contains('team-acco__item--active')) {
+      teamacco_items[i].classList.remove('team-acco__item--active');
+    } 
+    else {      
+      for (let j = 0; j < l; j++) {
+        teamacco_items[j].classList.remove('team-acco__item--active');
+      }  
+      
+      teamacco_items[i].classList.add('team-acco__item--active');
+    }
+  })
+};
