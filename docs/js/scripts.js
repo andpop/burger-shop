2004a6e9ba40f513d825fc037c7b902ebb04461f
Andrey Popov <andpop@mail.ru>
2018-05-17 20:18:31 +0300

    Папка docs подготовлена к просмотру

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -18,8 +18,7 @@ hamburgermenu_close.addEventListener('click', function(event) {
   document.body.style.overflow        = '';
 });
 
-var l = hamburgermenu_item_links.length;
-for (let i = 0; i<l; i++) {
+for (let i = 0; i<hamburgermenu_item_links.length; i++) {
   hamburgermenu_item_links[i].addEventListener('click', function(event) {
     hamburgermenu_content.style.display = '';
     document.body.style.overflow        = '';
@@ -28,8 +27,7 @@ for (let i = 0; i<l; i++) {
 
 // Обработка аккордеона в секции "Команда"
 var teamacco_items = document.querySelectorAll('.team-acco__item');
-var l              = teamacco_items.length;
-for (let i = 0; i<l; i++) {
+for (let i = 0; i<teamacco_items.length; i++) {
   teamacco_items[i].addEventListener('click', function(event) {
     event.preventDefault();
     event.stopPropagation();
@@ -37,11 +35,30 @@ for (let i = 0; i<l; i++) {
       teamacco_items[i].classList.remove('team-acco__item--active');
     } 
     else {      
-      for (let j = 0; j < l; j++) {
+      for (let j = 0; j < teamacco_items.length; j++) {
         teamacco_items[j].classList.remove('team-acco__item--active');
       }  
       
       teamacco_items[i].classList.add('team-acco__item--active');
+    }
+  })
+};
+
+// Обработка аккордеона в секции "Меню"
+var menuacco_items = document.querySelectorAll('.menu-acco__item');
+for (let i = 0; i<menuacco_items.length; i++) {
+  menuacco_items[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (menuacco_items[i].classList.contains('menu-acco__item--active')) {
+      menuacco_items[i].classList.remove('menu-acco__item--active');
+    } 
+    else {      
+      for (let j = 0; j < menuacco_items.length; j++) {
+        menuacco_items[j].classList.remove('menu-acco__item--active');
+      }  
+      
+      menuacco_items[i].classList.add('menu-acco__item--active');
     }
   })
 };
