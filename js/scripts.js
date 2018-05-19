374f2bfd91bd096f78a701c145748213db83d2d7
Andrey Popov <andpop@mail.ru>
2018-05-19 15:39:59 +0300

    Сделал popup для отзывов

--- js/scripts.js
+++ js/scripts.js
@@ -62,3 +62,56 @@ for (let i = 0; i<menuacco_items.length; i++) {
     }
   })
 };
+
+
+
+// Popup для полного текста отзывов
+const commentButtonList = document.querySelectorAll(".comments__btn");
+
+for (let commentButton of commentButtonList) {
+  commentButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    const full_comment = getFullCommentHTML(this);
+    const successPopup = createPopup(full_comment);
+    document.body.appendChild(successPopup);
+    document.body.style.overflow = 'hidden';
+
+});
+}
+
+function getFullCommentHTML(commentButton) {
+  let fullComment        = 'Полного комментария нет';
+  let fullCommentElement = commentButton.nextElementSibling;
+  if (!commentButton.classList.contains('comments__btn--mobile')) 
+    fullCommentElement = commentButton.nextElementSibling.nextElementSibling;
+  if (fullCommentElement) {
+    if (fullCommentElement.classList.contains('full_commentContent')) {
+      fullComment = fullCommentElement.innerHTML;
+    }
+  }
+  return fullComment;
+};
+
+function createPopup(content) {
+  const popupElement = document.createElement("div");
+  popupElement.classList.add("popup");
+
+  const popup                  = document.querySelector("#popupTemplate");
+        popupElement.innerHTML = popup.innerHTML;
+
+  const closeElement = popupElement.querySelector(".popup__close");
+  closeElement.addEventListener("click", function(event) {
+    event.preventDefault();
+    document.body.removeChild(popupElement);
+    document.body.style.overflow = '';
+
+  });
+  // const popupContainer = popupElement.querySelector(".popup__container");
+  // popupContainer.addEventListener("click", function () {
+  //   console.log("Click!");
+  // });
+  const contentElement           = popupElement.querySelector(".popup__content");
+        contentElement.innerHTML = content;
+
+  return popupElement;
+}
