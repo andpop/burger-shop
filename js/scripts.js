8999518008684e8586a0ac9ba17c4c58d8f30199
Andrey Popov <andpop@mail.ru>
2018-05-26 00:09:57 +0300

    Плавный вертикальный аккордеон. Начал делать карту

--- js/scripts.js
+++ js/scripts.js
@@ -369,3 +369,12 @@ btnOrder.on('submit', e => {
 });
 
 
+//============================== Карта Яндекс =============================================
+ymaps.ready(init);
+
+function init () {
+  var myMap = new ymaps.Map('map', {
+    center: [59.94, 30.32],
+    zoom  : 12
+  })
+};
